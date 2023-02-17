4cba140a3f019901d430e682929ac5409de5ad92
KatiaKost <katya-donya@mail.ru>
2023-02-17 17:40:55 +1000

    hw done

---
+++ src/HomeWork3.java
@@ -0,0 +1,52 @@
+public class HomeWork3 {
+    public static void main(String[] args) {
+        System.out.println("ЗАДАНИЕ 1-3");
+        double dog = 8.0;
+        double cat = 3.6;
+        int paper = 763789;
+        System.out.println("dog = " + dog + "; cat = " + cat + "; paper = " + paper);
+
+        dog = dog * 4;
+        cat = cat * 4;
+        paper = paper * 4;
+        System.out.println("dog = " + dog + "; cat = " + cat + "; paper = " + paper);
+
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println("dog = " + dog + "; cat = " + cat + "; paper = " + paper);
+
+        System.out.println();
+
+        System.out.println("ЗАДАНИЕ 4");
+        int friend = 19;
+        System.out.println("friend = " + friend);
+        friend = friend * 2;
+        System.out.println("friend = " + friend);
+        friend = friend / 7;
+        System.out.println("friend = " + friend);
+
+        System.out.println();
+
+        System.out.println("ЗАДАНИЕ 5");
+        double frog = 3.5;
+        System.out.println("frog = " + frog);
+        frog = frog * 10;
+        System.out.println("frog = " + frog);
+        frog = frog / 3.5;
+        System.out.println("frog = " + frog);
+
+        System.out.println();
+
+        System.out.println("ЗАДАНИЕ 6");
+        double weightOfFirstBoxer = 78.2;
+        double weightOfSecondBoxer = 82.7;
+        System.out.println("Вес первого боксера = " + weightOfFirstBoxer + "; Вес второго боксера = " + weightOfSecondBoxer);
+        double totalWeightOfBoxers = weightOfFirstBoxer + weightOfSecondBoxer;
+        System.out.println("Общей вес двух бойцов = " + totalWeightOfBoxers);
+        double boxerWeightDifference = weightOfSecondBoxer - weightOfFirstBoxer;
+        System.out.println("Разница в весе боксеров = " + boxerWeightDifference);
+
+
+    }
+}
